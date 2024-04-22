76f394fe2f439b7464a2d1088d8507ded6ebe318
lebotreasure <lebohangsylviak@gmail.com>
2024-04-22 12:32:08 +0200

    Added validation

--- src/main/java/com/enviro/assessment/grad001/lebohangkhaeane/entity/Category.java
+++ src/main/java/com/enviro/assessment/grad001/lebohangkhaeane/entity/Category.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,7 +19,11 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "Content cannot be blank")
     private String name;
+
+    @NotBlank(message = "Content cannot be blank")
+    @Size(max = 255)
     private String description;
 
 }
