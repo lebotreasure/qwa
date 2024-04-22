4bc08bf9a000032555d66015ae3348e6850ce40e
lebotreasure <lebohangsylviak@gmail.com>
2024-04-22 10:25:34 +0200

    Initialise category interface

---
+++ src/main/java/com/enviro/assessment/grad001/lebohangkhaeane/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.enviro.assessment.grad001.lebohangkhaeane.repository;
+
+import com.enviro.assessment.grad001.lebohangkhaeane.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
