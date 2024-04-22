a4259a1ab40412cabf7280d8b0375b11e9f5dbb4
lebotreasure <lebohangsylviak@gmail.com>
2024-04-22 10:23:43 +0200

    Initial commit

---
+++ src/main/java/com/enviro/assessment/grad001/lebohangkhaeane/LebohangkhaeaneApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.lebohangkhaeane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LebohangkhaeaneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LebohangkhaeaneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enviro/assessment/grad001/lebohangkhaeane/LebohangkhaeaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.lebohangkhaeane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LebohangkhaeaneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
